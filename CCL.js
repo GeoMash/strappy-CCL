95df121e7e6a2963db1828ba52d5c880f98ca605
Timothy Chandler <tim@s3.net.au>
2012-09-11 15:16:59 +0800

    Common CCL file.

---
+++ CCL.js
@@ -0,0 +1,21 @@
+$JSKK.Class.create
+(
+	{
+		$namespace:	'strappy',
+		$name:		'CCL'
+	}
+)
+(
+	{
+		version:			'1.0.0',
+		Signal:
+		{
+			SWITCH:			'strappy.signal.ccl.switch'
+		},
+		Type:
+		{
+			INSTRUCTION:	'strappy.signal.ccl.instruction'
+		}
+	},
+	{}
+);
