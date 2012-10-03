2979d0b6825c6d90ac44ff5fa6ae3d82c024ab6a
Luke Hoggett <luke.hoggett@spinifexgroup.com>
2012-10-03 11:32:06 +1000

    added basic html rendering

--- components/container/controller/HTMLLayout.js
+++ components/container/controller/HTMLLayout.js
@@ -1,25 +1,27 @@
 $JSKK.Class.create
-(
+	(
 	{
-		$namespace:	'strappy.ccl.container.controller',
+	$namespace:	'strappy.ccl.container.controller',
 		$name:		'HTMLLayout',
 		$extends:	strappy.mvc.Controller
 	}
-)
-(
+	)
+	(
 	{},
 	{
 		init: function()
 		{
 			this.init.$parent();
-			if (this.getConfig('layout')=='html')
+			if (this.getConfig('layout') === 'html')
 			{
-				
+				var view = this.getView('Default');
+				view.getContainer().append(this.getConfig('html'));
+				view.show();
 			}
 		},
 		onChangeCard: function(signal)
 		{
-			
+
 		}
 	}
-);
+	);
