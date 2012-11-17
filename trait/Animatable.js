fc31b8c58dc104e91164d86738acbf81a5e4626a
Timothy Chandler <tim@s3.net.au>
2012-11-17 23:22:38 +0800

    New Animatable Trait.

---
+++ trait/Animatable.js
@@ -0,0 +1,35 @@
+$JSKK.Trait.create
+(
+	{
+		$namespace:	'strappy.CCL.trait',
+		$name:		'Animatable'
+	}
+)
+(
+	{
+		enableAnimations: function()
+		{
+			this.getContainer().css
+			(
+				{
+					transition:				'all .2s linear',
+					'-o-transition':		'all .2s linear',
+					'-moz-transition':		'all .2s linear',
+					'-webkit-transition':	'all .2s linear'
+				}
+			);
+		},
+		disableAnimations: function()
+		{
+			this.getContainer().css
+			(
+				{
+					transition:				'all 0s linear',
+					'-o-transition':		'all 0s linear',
+					'-moz-transition':		'all 0s linear',
+					'-webkit-transition':	'all 0s linear'
+				}
+			);
+		}
+	}
+);
