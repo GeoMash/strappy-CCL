e47fdeccbc216de44c1c3ed4a7fadad5c00e723e
unknown <tim_000@.(none)>
2013-03-24 16:10:33 +0800

    requirejs integration

---
+++ main.js
@@ -0,0 +1,24 @@
+define
+(
+	'strappy-ccl',
+	[
+		'./CCL',
+		'./trait/Containable',
+		'./trait/Animatable',
+		'./trait/Pluploadable',
+		'./component/application/Controller',
+		'./controller/Router',
+		'./component/container/Container',
+		'./component/container/controller/State',
+		'./component/container/controller/AutoLayout',
+		'./component/container/controller/CardLayout',
+		'./component/container/controller/HTMLLayout',
+		'./component/container/view/Default',
+		'./component/container/store/State',
+		'./component/uploader/Uploader',
+		'./component/uploader/controller/Default',
+		'./component/uploader/controller/State',
+		'./component/uploader/view/Default',
+		'./component/uploader/store/State'
+	]
+);
