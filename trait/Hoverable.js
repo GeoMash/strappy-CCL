238580bb399a4fb190afe8a5dc0687f27c0547b5
simon ilett <simon.ilett@gmail.com>
2013-09-23 18:50:52 +0800

    new trait

---
+++ trait/Hoverable.js
@@ -0,0 +1,76 @@
+$JSKK.Trait.create
+(
+	{
+		$namespace:	'strappy.ccl.trait',
+		$name:		'Hoverable'
+	}
+)
+(
+	{
+		hoverSetUp: function(el, settings)
+		{
+			//console.log($.extend({}, this.hoverSettings, settings))
+			$(el).hoverIntent
+			(
+				$.extend({}, this.hoverSettings(), settings)
+			);
+		},
+		hoverSettings: function()
+		{
+			// Defaults
+			return {
+				interval: 200,
+				sensitivity: 4,
+				timeout: 200,
+				over: this.addHoverMenu,
+				out: this.removeHoverMenu
+			};
+		},
+		addHoverMenu: function(e)
+		{
+			var 
+				parentContainer 	= $(e.currentTarget || e),
+				menu 				= parentContainer.find('.'+parentContainer.data('hover'));  
+
+			if (parentContainer)
+			{
+				parentContainer.addClass('is-active');
+				parentContainer.find('.jspscroll_delayed').jScrollPane({horizontalDragMaxWidth: 0, verticalGutter: 3});
+				if(menu) {
+					menu.animate({opacity:1},250);
+				} 
+			} 
+			else 
+			{
+				console.log('either the menu or the parent does not exist');
+			}
+		},
+		removeHoverMenu: function()
+		{
+			var 
+				parentContainer 	= $(this),
+				menu 				= parentContainer.find('.'+parentContainer.data('hover'));  
+
+			if (parentContainer)
+			{
+				menu.animate
+				(
+					{opacity:0},
+					250,
+					function()
+					{
+						parentContainer.removeClass('is-active');
+					}
+				);
+			}
+			else 
+			{
+				console.log('either the menu or the parent does not exist');
+			}
+		},
+		getHoverMenu: function(el)
+		{
+			return (el.find('.'+el.data('hover')));  
+		}
+	}
+);
