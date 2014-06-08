815a087ed3f93ecbd9321a0296611557fa861eaf
Timothy Chandler <tim@praxisbt.com>
2014-06-08 23:29:36 +0800

    Added a new trait for easily finding child components of any nesting level.

---
+++ trait/Traversable.js
@@ -0,0 +1,34 @@
+/**
+ * This trait allows you to easily fetch any nested child component
+ * simply by giving it a dot notated string of component references.
+ * 
+ * This trait works particularly well with the containable trait.
+ * 
+ * @class strappy.ccl.trait.Traversable
+ */
+$JSKK.Trait.create
+(
+	{
+		$namespace:	'strappy.ccl.trait',
+		$name:		'Traversable'
+	}
+)
+(
+	{
+		getChildCmp: function(selector)
+		{
+			var parts	=String(selector).split('.'),
+				cmp		=this;
+			for (var i=0,j=parts.length; i<j; i++)
+			{
+				cmp=cmp.getCmp(parts[i]);
+				console.debug(cmp);
+				if (Object.isUndefined(cmp))
+				{
+					return null;
+				}
+			}
+			return cmp;
+		}
+	}
+);
